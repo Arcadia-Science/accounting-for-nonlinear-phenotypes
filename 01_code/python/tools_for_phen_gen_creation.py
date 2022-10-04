23d9d24236c4804da0d5a099de6cfcfc32986310
Dave Mets <dgmets@gmail.com>
2022-10-04 15:32:21 -0700

    Add variable numbers of phenotypes fo gen/phen creation function

--- 01_code/python/tools_for_phen_gen_creation.py
+++ 01_code/python/tools_for_phen_gen_creation.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-def make_genotype(n_as=None,n_loci=None, n_loci_ip=None, n_animals=None):
+
+
+def make_genotype(n_as=None,n_loci=None, n_loci_ip=None, n_animals=None,n_phens=None):
  '''a simple tool for generating phenotypic and genetic data.  Currently,
  the genetic model is purely additive. 
  n_as is the number of allelic states at all segregating loci
@@ -11,22 +13,27 @@ def make_genotype(n_as=None,n_loci=None, n_loci_ip=None, n_animals=None):
  if n_loci==None: n_loci=3000
  if n_loci_ip==None: n_loci_ip=10
  if n_animals==None: n_animals=500 
+ if n_phens==None: n_phens=1
 
  loci_that_matter=np.zeros(n_loci)
- for n in range(n_alleles_that_matter):
+ for n in range(n_loci_ip):
   loci_that_matter[np.random.randint(n_loci)]=1
 
- weights=np.zeros((n_loci,n_allelic_states))
- for n in range(n_alleles_that_matter):
-  weights[np.random.randint(n_loci)]=np.random.rand(n_allelic_states)
+ weights=np.zeros((n_loci,n_as))
+ for n in range(n_loci_ip):
+  weights[np.random.randint(n_loci)]=np.random.rand(n_as)
 
- genotypes,gen_locs=zip(*[make_genotype(n_allelic_states,n_loci) for x in range(n_animals)])
+ genotypes,gen_locs=zip(*[make_genotype_ind(n_as,n_loci) for x in range(n_animals)])
 
- phens=np.sum(genotypes*weights,axis=(2,1))/n_loci
+ phens=[]
+ for n in range(n_phens):
+  phens.append(np.sum(genotypes*weights,axis=(2,1))/n_loci)
+
  return genotypes,gen_locs,phens
 
 def make_genotype_ind(n_as,n_loci):
- '''utlity function for make_genotype'''
+ '''utlity function for make_genotype.  Creates a random set of genotypes
+ for an individual.'''
  gen=np.zeros((n_loci,n_as))
  locs=[]
  for n in range(n_loci):
